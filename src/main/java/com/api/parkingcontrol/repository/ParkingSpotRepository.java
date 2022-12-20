565130ce8d626486c9ef46f84c20b51f8394d559
Luis Felipe Guimarães Pinto <felipeguimaraes@MacBook-de-Luis-Felipe.local>
2022-12-20 15:01:21 -0300

    Inicial Commit

---
+++ src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
@@ -0,0 +1,13 @@
+package com.api.parkingcontrol.repository;
+
+import com.api.parkingcontrol.model.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+}
